feb6087a95c9da932cd570e17a082278fe221137
ssnajjar <ssnajjar05@gmail.com>
2025-02-18 16:55:23 -0600

    creating example for routes in the server file

---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+//server.js 
+const http = require('http')
+const { testing } = require('./testing')
+const { testingPost } = require('./testingPost')
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/api/testing' && req.method === 'GET') {
+        testing(req, res)
+    }
+    else if(req.url === '/api/testingPost' && req.method === 'POST'){
+        testingPost(req, res)
+    }
+    else{
+        res.writeHead(404, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify({ message: 'Route not found'}))
+    }
+
+
+})
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
