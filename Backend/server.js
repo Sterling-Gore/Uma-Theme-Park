35dcec1da68dbb0e6e2b3fcb5912da8f51666739
ssnajjar <ssnajjar05@gmail.com>
2025-02-20 13:54:21 -0600

    adding cors as a dependency. Allowing the front end to speak to the back end by adding cors headers. Adding the createCustomer route. Creating the handleSubmit function in the register js file

--- Backend/server.js
+++ Backend/server.js
@@ -1,30 +1,39 @@
-//server.js 
-const http = require('http')
-const { testing } = require('./testing')
-const { testingPost } = require('./testingPost')
+// server.js 
+const http = require('http');
+const { testing } = require('./testing');
+const { testingPost } = require('./testingPost');
+const { registerCustomer } = require('./registerCustomer');
+
+const PORT = process.env.PORT || 4000;
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/api/testing' && req.method === 'GET') {
-        testing(req, res)
+    // Set CORS Headers
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return;
     }
-    else if(req.url === '/api/testingPost' && req.method === 'POST'){
-        testingPost(req, res)
+
+    // DEFINE ROUTES HERE 
+    if (req.url === '/api/testing' && req.method === 'GET') { 
+        testing(req, res);
     }
-    else{
-        res.writeHead(404, {'Content-Type': 'application/json'})
-        res.end(JSON.stringify({ message: 'Route not found'}))
+    else if (req.url === '/api/testingPost' && req.method === 'POST'){
+        testingPost(req, res);
     }
-
-
-
-
-
-
-
+    else if (req.url === '/customer/createCustomer' && req.method === 'POST'){
+        registerCustomer(req, res);
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Route not found' }));
+    }
 
     
-})
+});
 
-const PORT = process.env.PORT || 3000
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
