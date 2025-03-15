53e0cb986d0a9e462ecb37c31076f7a5b2b03231
Sterling-Gore <155609125+Sterling-Gore@users.noreply.github.com>
2025-03-15 03:17:27 -0500

    added in the logic for updating price and stock of merchandise (#71)

--- Frontend/src/pages/Employee/components/HandleMerchandise.js
+++ Frontend/src/pages/Employee/components/HandleMerchandise.js
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "../../../App.css";
 
 
 const HandleMerchandise = ({ setActiveTab }) => {
+    const alertShown = useRef(false);
     const [merchandises, setMerchandises] = useState([]);
     const [refreshMerchandise, setRefreshMerchandise] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [newStockAmount, setNewStockAmount] = useState("");
+    const [newPrice, setNewPrice] = useState("");
 
 
 
@@ -26,9 +30,16 @@ const HandleMerchandise = ({ setActiveTab }) => {
             const data = await response.json();
             if (data.success) {
                 //setEmployees(data.data);
+                const merch = data.data.map((item,index) => {
+                    return {
+                        ...item,
+                        is_editing_stock : false,
+                        is_editing_price : false
+                    };
+                });
 
-                setMerchandises(data.data);
-                console.log(data.data);
+                setMerchandises(merch);
+                console.log(merch);
 
                 //console.log(merch);
                 
@@ -41,6 +52,138 @@ const HandleMerchandise = ({ setActiveTab }) => {
         fetchMerchandise();
         
     }, [refreshMerchandise]);
+
+    const EditMerchandiseStock = (merchandise_id) => {
+        setIsEditing(true);
+        const newMerchandise = merchandises.map(item => {
+            if (item.merchandise_id === merchandise_id)
+            {
+                item.is_editing_stock = true;
+            }
+            return item;
+        });
+        setMerchandises(newMerchandise);
+    };
+
+    const cancelEditMerchandiseStock = (merchandise_id) => {
+        setIsEditing(false);
+        setNewStockAmount("");
+        const newMerchandise = merchandises.map(item => {
+            if (item.merchandise_id === merchandise_id)
+            {
+                item.is_editing_stock = false;
+            }
+            return item;
+        });
+        setMerchandises(newMerchandise);
+    };
+
+    const submitEditMerchandiseStock = async (merchandise_id) => {
+        try {
+            const dataToSend = {
+                id : merchandise_id,
+                newStock : Number(newStockAmount)
+            }
+            const response = await fetch('http://localhost:4000/updateMerchandiseStock', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dataToSend),
+        });
+        
+        if (!response.ok) {
+            throw new Error('Failed to update merchandise');
+        }
+        
+        setIsEditing(false);
+        setNewStockAmount("");
+        /*
+        const newMerchandise = merchandises.map(item => {
+            if (item.merchandise_id === merchandise_id)
+            {
+                item.is_editing_stock = false;
+            }
+            return item;
+        });
+        setMerchandises(newMerchandise);*/
+        setRefreshMerchandise(!refreshMerchandise);
+
+        } catch (error) {
+            console.error('Error fetching merchandise:', error);
+            alertShown.current = true;
+            alert("Error updating merchandise");
+        }
+        
+
+    }
+
+    const EditMerchandisePrice = (merchandise_id) => {
+        setIsEditing(true);
+        const newMerchandise = merchandises.map(item => {
+            if (item.merchandise_id === merchandise_id)
+            {
+                item.is_editing_price = true;
+            }
+            return item;
+        });
+        setMerchandises(newMerchandise);
+    };
+
+    const cancelEditMerchandisePrice = (merchandise_id) => {
+        setIsEditing(false);
+        setNewPrice("");
+        const newMerchandise = merchandises.map(item => {
+            if (item.merchandise_id === merchandise_id)
+            {
+                item.is_editing_price = false;
+            }
+            return item;
+        });
+        setMerchandises(newMerchandise);
+    };
+
+    const submitEditMerchandisePrice = async (merchandise_id) => {
+        try {
+            const dataToSend = {
+                id : merchandise_id,
+                newPrice : Number(newPrice)
+            }
+            const response = await fetch('http://localhost:4000/updateMerchandisePrice', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dataToSend),
+        });
+        
+        if (!response.ok) {
+            throw new Error('Failed to update merchandise');
+        }
+        
+        setIsEditing(false);
+        setNewPrice("");
+        /*
+        const newMerchandise = merchandises.map(item => {
+            if (item.merchandise_id === merchandise_id)
+            {
+                item.is_editing_stock = false;
+            }
+            return item;
+        });
+        setMerchandises(newMerchandise);*/
+        setRefreshMerchandise(!refreshMerchandise);
+
+        } catch (error) {
+            console.error('Error fetching merchandise:', error);
+            alertShown.current = true;
+            alert("Error updating merchandise");
+        }
+        
+
+    }
 
 
 
@@ -62,7 +205,6 @@ const HandleMerchandise = ({ setActiveTab }) => {
                                 
                                 <><strong>Stock Amount:</strong> {Merchandise.stock_amount}</>
                                 
-                                
                             </span>
                             <span className="attraction-detail">
                              {/*}   {Merchandise.in_shopping_cart > 0 && (
@@ -72,15 +214,79 @@ const HandleMerchandise = ({ setActiveTab }) => {
                         </div>
 
                         <div className="attraction-footer">
-                            <button className="attraction-button" /*onClick={() => addToCart(Merchandise.merchandise_id)}*/>
+                            {!isEditing && (
+                            <button className="attraction-button" onClick={() => EditMerchandiseStock(Merchandise.merchandise_id)}>
                                 Update Stock Amount
                             </button>
+                            )}
+                            {Merchandise.is_editing_stock && (
+                                <>
+                                <label className="label-header"> Set Stock Amount </label>
+                                <input
+                                type="text"
+                                value={newStockAmount}
+                                onChange={(e) => {
+                                    let value = e.target.value.replace(/\D/g, ""); 
+                                    setNewStockAmount(value);
+                                    //setCardHolder(onlyLetters);
+                                    //checkError();
+                                }}   
+                                placeholder="Stock Amount"
+                                maxLength="3"
+                                minLength="3"
+                                required
+                                />
+                                {newStockAmount.length > 0 && (
+                                    <button className="attraction-button" onClick={() => submitEditMerchandiseStock(Merchandise.merchandise_id)}>
+                                        Confirm Changes
+                                    </button>
+                                )}
+                                <button className="attraction-button" onClick={() => cancelEditMerchandiseStock(Merchandise.merchandise_id) }>
+                                    Cancel Changes
+                                </button>
+                                </>
+                            )}
+                        </div>
+
+                        <div className="attraction-footer">
+                            {!isEditing && (
+                            <button className="attraction-button" onClick={() => EditMerchandisePrice(Merchandise.merchandise_id)}>
+                                Update Price
+                            </button>
+                            )}
+                            {Merchandise.is_editing_price && (
+                                <>
+                                <label className="label-header"> Set Price </label>
+                                <input
+                                type="text"
+                                value={newPrice}
+                                onChange={(e) => {
+                                    let value = e.target.value.replace(/\D/g, ""); 
+                                    setNewPrice(value);
+                                    //setCardHolder(onlyLetters);
+                                    //checkError();
+                                }}   
+                                placeholder="Price"
+                                maxLength="3"
+                                minLength="3"
+                                required
+                                />
+                                {newPrice.length > 0 && (
+                                    <button className="attraction-button" onClick={() => submitEditMerchandisePrice(Merchandise.merchandise_id)}>
+                                        Confirm Changes
+                                    </button>
+                                )}
+                                <button className="attraction-button" onClick={() => cancelEditMerchandisePrice(Merchandise.merchandise_id) }>
+                                    Cancel Changes
+                                </button>
+                                </>
+                            )}
                         </div>
                     </div>
                 </div>
             ))}
         </div>
-      <button onClick={() => setActiveTab('dashboard')}>Back to Dashboard</button>
+      {/*<button onClick={() => setActiveTab('dashboard')}>Back to Dashboard</button>*/}
     </div>
   );
 };
