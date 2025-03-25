4b318dd6ca0cef55326c6a9e2635f87880df080d
ssnajjar <ssnajjar05@gmail.com>
2025-03-24 21:35:47 -0500

    server file fix

--- Backend/server.js
+++ Backend/server.js
@@ -48,7 +48,12 @@ const allowedOrigins = ['http://localhost:3000', 'https://bluehorizonadventures.
 
 const server = http.createServer(async (req, res) => {
     // Set CORS Headers
-    res.setHeader('Access-Control-Allow-Origin', allowedOrigins);
+
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    //res.setHeader('Access-Control-Allow-Origin', allowedOrigins);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     //res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
