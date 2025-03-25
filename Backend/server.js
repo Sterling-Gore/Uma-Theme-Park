8c5b48e8db174a323f593782b5ab9c9c1469bd4a
ssnajjar <ssnajjar05@gmail.com>
2025-03-24 21:28:27 -0500

    fixing server file for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -44,9 +44,11 @@ const { updateTaskStatus } = require('./employeePortal/updateTaskStatus')
 
 const PORT = process.env.PORT || 7000;
 
+const allowedOrigins = ['http://localhost:3000', 'https://bluehorizonadventures.vercel.app'];
+
 const server = http.createServer(async (req, res) => {
     // Set CORS Headers
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', allowedOrigins);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     //res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
